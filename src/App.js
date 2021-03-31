90225da77f9d06736dcc48631d1b6fda7b43c7eb
NaruboinaYadav <naruboina.yadav@ltts.com>
2021-03-31 23:40:22 +0530

    First React Demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import ListComponent from './Components/ListComponent'
+import ListEmployee from './Components/ListEmployee'
+import CreateEmployee from './Components/CreateEmployee'
+import ViewEmployee from './Components/ViewEmployee'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <div className = "container">
+          <Switch>
+            <Route path = "/" exact component = {ListEmployee}></Route>
+            <Route path = "/employees" component = {ListEmployee}></Route>
+            <Route path = "/add-employee/:id" component = {CreateEmployee}></Route>
+            <Route path = "/view-employee/:id" component = {ViewEmployee}></Route>
+          </Switch>
+
+        </div>
+      </Router>
+      
     </div>
   );
 }
